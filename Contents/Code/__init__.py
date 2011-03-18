ccd10ddf7246500ca060f19a7fbd487e4973cdb2
Pierre <p.dellanave@gmail.com>
2011-03-17 23:41:41 -0700

    Rewrite to accomodate site change + conversion to v2

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,3 @@
-# PMS plugin framework
-from PMS import *
-
 ####################################################################################################
 
 VIDEO_PREFIX = "/video/lynda"
@@ -12,10 +9,11 @@ ICON = 'icon-default.png'
 SEARCH = 'icon-search.png'
 PREFS = 'icon-prefs.png'
 
+filters = {'Subjects':'u','Software':'s','Software Company':'c','Author':'a','Learning Level':'l'}
+
 ####################################################################################################
 
 def Start():
-    HTTP.__cookieJar.clear()
 
     Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
 
@@ -27,17 +25,20 @@ def Start():
     DirectoryItem.art = R(ART)
     DirectoryItem.thumb = R(ICON)
     VideoItem.thumb = R(ICON)
-    HTTP.SetcacheTime = CACHE_1HOUR
-    HTTP.__headers["User-agent"] = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-gb) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16"  
+    HTTP.CacheTime = CACHE_1HOUR
+    HTTP.Headers["User-agent"] = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_6; en-gb) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16"  
 
-def CreatePrefs():
-    Prefs.Add(id='username', type='text', default='', label='Your Username')
-    Prefs.Add(id='password', type='text', default='', label='Your Password', option='hidden')
-    Prefs.Add(id='autologin', type='bool', default='False', label='Remember me')
+    page_content = HTTP.Request('http://www.lynda.com/allcourses').content
+    
+    data = page_content[page_content.find("var data = ")+11:]
+    data = data[:data.find('};')+1]
+    Log(data)
+    
+    Dict['courseCatalog'] = JSON.ObjectFromString(data)
 
 def ValidatePrefs():
-    u = Prefs.Get('username')
-    p = Prefs.Get('password')
+    u = Prefs['username']
+    p = Prefs['password']
     if (u and p):
       L = Login(username=u,password=p)
       Log(L)
@@ -50,9 +51,13 @@ def Login(username,password):
   valuesLogIn = dict()
   valuesLogOut = dict()
   alreadyLoggedIn = False
-
-  address = XML.ElementFromURL('http://www.lynda.com/login/LoginError.aspx',True,cacheTime=0).xpath('//link[@rel="canonical"]')[0].get('href')
-  for input in  XML.ElementFromURL(address,True,cacheTime=0).xpath('//form[@id="aspnetForm"]//input[@name]'):
+  
+  try:
+    address = HTML.ElementFromURL('http://www.lynda.com/login/LoginError.aspx',cacheTime=0).xpath('//link[@rel="canonical"]')[0].get('href')
+  except: 
+    return False
+    
+  for input in  HTML.ElementFromURL(address,cacheTime=0).xpath('//form[@id="aspnetForm"]//input[@name]'):
     field_name = input.get('name')
     Log(field_name)
     value = input.get('value')
@@ -74,7 +79,7 @@ def Login(username,password):
         valuesLogIn[field_name] = value
 
   if alreadyLoggedIn == False:
-    response = HTTP.Request('http://www.lynda.com/login/LoginError.aspx',values=valuesLogIn,cacheTime=0)
+    response = HTTP.Request('http://www.lynda.com/login/LoginError.aspx',values=valuesLogIn,cacheTime=0).content
 
     if (response.find('Login Error')==-1) & (response.find('An Error Has Occurred')==-1) & (response!=None):
       return True
@@ -90,56 +95,52 @@ def VideoMainMenu():
 
     dir.Append(Function(DirectoryItem(CourseList,"All Courses",subtitle="Browse all the available courses in the Online Training Library",summary="Browse all the available courses in the Online Training Library")))
 
-    dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Subject ...",subtitle="Browse the Online Training Library by subject",summary="Browse the Online Training Library by subject"),BrowseCategory = 'Subject'))
-    dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Software ...",subtitle="Browse the Online Training Library by software",summary="Browse the Online Training Library by software"),BrowseCategory = 'Products'))
-    dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Vendor ...",subtitle="Browse the Online Training Library by vendor",summary="Browse the Online Training Library by Vendor"),BrowseCategory = 'Vendor'))
+    dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Subject ...",subtitle="Browse the Online Training Library by subject",summary="Browse the Online Training Library by subject"),BrowseCategory = 'Subjects'))
+    dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Software ...",subtitle="Browse the Online Training Library by software",summary="Browse the Online Training Library by software"),BrowseCategory = 'Software'))
+    dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Vendor ...",subtitle="Browse the Online Training Library by vendor",summary="Browse the Online Training Library by Vendor"),BrowseCategory = 'Software Company'))
     dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Author ...",subtitle="Browse the Online Training Library by author",summary="Browse the Online Training Library by author"),BrowseCategory = 'Author'))
+    dir.Append(Function(DirectoryItem(BrowseBy,"Browse by Learning Level ...",subtitle="Browse the Online Training Library by learning level",summary="Browse the Online Training Library by learning level"),BrowseCategory = 'Learning Level'))
 
     dir.Append(Function(InputDirectoryItem(SearchResults,"Search ...","Search  the Online Training Library",summary="Search  the Online Training Library",thumb=R(SEARCH))))
     dir.Append(PrefsItem(title="Preferences",subtile="",summary="Plug-in preferences",thumb=R(PREFS)))
   
-    u = Prefs.Get('username')
-    p = Prefs.Get('password')
-    auto = Prefs.Get('autologin')
+    u = Prefs['username']
+    p = Prefs['password']
+    auto = Prefs['autologin']
     if (u and p and auto):
       Login(username=u,password=p)
-    #  if Login(username=u,password=p) == True:
-	#    return MessageContainer("Success","You have been logged in automatically, please enjoy the videos")
-    #  else:
-    #    return MessageContainer("Error","You need to provide a valid username and password in the Preferences menu") 
     return dir
     
 def BrowseBy(sender, BrowseCategory):
     dir = MediaContainer(viewGroup="List")
-    tableContent = XML.ElementFromURL('http://www.lynda.com/home/ViewCourses.aspx?lpk3=true&PageNum=0',True).xpath('//select[@id="ddl'+BrowseCategory+'"]/option[@value!="-1"]')
-    
-    for entry in tableContent:
-      dir.Append(Function(DirectoryItem(CourseList,entry.text),listType='lpk0='+str(entry.get("value"))))
-
+    for entry in Dict['courseCatalog']['sections'][BrowseCategory]:
+      name = Dict['courseCatalog']['sections'][BrowseCategory][str(entry)]['name']
+      if name != '':
+        sortindex = Dict['courseCatalog']['sections'][BrowseCategory][str(entry)]['sort']
+        Log(entry)
+        dir.Append(Function(DirectoryItem(CourseList,name,subtitle = sortindex),filterKey=filters[BrowseCategory],filterVal = entry))
+        dir.Sort('subtitle')
     return dir
 
-def CourseList(sender, PageNum = 0, pagesEnabled = False, listType= 'lpk3=true'):
-
+def CourseList(sender,filterKey='', filterVal=0, PageNum = 0, pagesEnabled = False, listType= 'lpk3=true'):
     dir = MediaContainer(viewGroup="List")
-    tableContent = XML.ElementFromURL('http://www.lynda.com/home/ViewCourses.aspx?%s&PageNum=%s' %(listType, PageNum),True).xpath('//div[@id="ctl00_ContentPlaceHolder2_UcHeaderSearchResults1_divResults"]//td[@class="l"]//a')
-    if PageNum >0:
-	    dir.Append(Function(DirectoryItem(CourseList,"Previous Page"),PageNum = PageNum - 1))
-
-    for entry in tableContent:
-      courseid = entry.get('cid')
-      if courseid != None:
-        dir.Append(Function(DirectoryItem(CourseDetails,entry.text),courseid = courseid))
-      
-    if pagesEnabled == True:
-      dir.Append(Function(DirectoryItem(CourseList,"Next Page"),PageNum = PageNum + 1))
-    
+    for datadict in Dict['courseCatalog']['courses']:
+      for key, value in datadict.items():
+        try:
+          for val in value:
+            if key == filterKey and val == int(filterVal) :
+              dir.Append(Function(DirectoryItem(CourseDetails,datadict["title"]),courseid = datadict["cid"]))
+        except:
+          if key == filterKey and value == int(filterVal) :
+              dir.Append(Function(DirectoryItem(CourseDetails,datadict["title"]),courseid = datadict["cid"]))
+            
     return dir
 
 def CourseDetails(sender, courseid = 0):
 
     dir = MediaContainer(viewGroup="List")
-    html = HTTP.Request('http://www.lynda.com/home/CourseDetails.ashx?fn=1&cid=%s&aid=67&cc=&_=1285450832173&{} '%courseid,cacheTime=0).replace('<span','<div id="chapter"><span').replace('</table>','</table></div>').replace('<table id="head"','<div><table id="head"')
-    Content = XML.ElementFromString(html,True).xpath('//div[@id="chapter"]')
+    html = HTTP.Request('http://www.lynda.com/home/CourseDetails.ashx?fn=1&cid=%s&aid=67&cc=&_=1285450832173&{} '%courseid,cacheTime=0).content.replace('<span','<div id="chapter"><span').replace('</table>','</table></div>').replace('<table id="head"','<div><table id="head"')
+    Content = HTML.ElementFromString(html).xpath('//div[@id="chapter"]')
     for entry in Content:
       chapterTitle = entry.xpath('a[@class="a"]')[0].text.strip()
       for course in entry.xpath('table//a'):
@@ -155,7 +156,7 @@ def CourseDetails(sender, courseid = 0):
     return dir
 
 def PlayVideo(sender, chapterid):
-     return Redirect(XML.ElementFromURL('http://www.lynda.com/home/GetPlayerXML.aspx?lpk4=%s' % chapterid,False).xpath('//flv')[0].text)
+     return Redirect(XML.ElementFromURL('http://www.lynda.com/home/GetPlayerXML.aspx?lpk4=%s' % chapterid).xpath('//flv')[0].text)
 
 def PopupMessage(sender):
     return MessageContainer(
@@ -165,7 +166,7 @@ def PopupMessage(sender):
 
 def SearchResults(sender,query=None):
 
-    html = HTTP.Request('http://www.lynda.com/ajax/search.aspx?q=%s&page=1&s=relevance&sa=true&f=producttypeid:4' % query)
+    html = HTTP.Request('http://www.lynda.com/ajax/search.aspx?q=%s&page=1&s=relevance&sa=true&f=producttypeid:4' % query).content
     Content = JSON.ObjectFromString(html)
     if (Content['results']['count']==0):
       return MessageContainer("No Result","Please try again")
